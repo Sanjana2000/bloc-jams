4c1ab62eed6185b8d93ea19e8bc7aee71260086b
Sanjana2000 <sanjana100@gmail.com>
2017-04-11 11:42:31 -0700

    modified album.js

---
+++ scripts/album.js
@@ -0,0 +1,131 @@
+// These are two album objects ( my temporary database)for this Album.In real senario,I'll pull hundreds or thousands from other database.These objects store info about songs,ex;title,artist,label,songs etc.
+
+// Example Album
+ var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: '4:26' },
+         { title: 'Green', duration: '3:14' },
+         { title: 'Red', duration: '5:01' },
+         { title: 'Pink', duration: '3:21'},
+         { title: 'Magenta', duration: '2:15'}
+     ]
+ };
+
+// Another Example Album
+ var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
+
+var createSongRow = function(songNumber, songName, songLength) {
+     var template =
+        '<tr class="album-view-song-item">'
+        + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
+
+      + '  <td class="song-item-title">' + songName + '</td>'
+      + '  <td class="song-item-duration">' + songLength + '</td>'
+      + '</tr>';
+ 
+     return template;
+ };
+
+  var setCurrentAlbum = function(album) {
+     // #1
+     var albumTitle = document.getElementsByClassName('album-view-title')[0];
+     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+     var albumImage = document.getElementsByClassName('album-cover-art')[0];
+     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+ 
+     // #2
+     albumTitle.firstChild.nodeValue = album.title;
+     albumArtist.firstChild.nodeValue = album.artist;
+     albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
+     albumImage.setAttribute('src', album.albumArtUrl);
+ 
+     // #3
+     albumSongList.innerHTML = '';
+	
+	// #4
+     for (var i = 0; i < album.songs.length; i++) {
+         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+     }
+ };
+  
+   var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+   var songRows = document.getElementsByClassName('album-view-song-item');
+
+   // Album button templates
+ var playButtonTemplate = '<a class="album-song-button"> <span class="ion-play"></span></a>';
+ 
+ window.onload = function() {
+     setCurrentAlbum(albumPicasso);
+	 songListContainer.addEventListener('mouseover', function(event) {
+         // #1
+         // Only target individual song rows during event delegation
+         if (event.target.parentElement.className === 'album-view-song-item') {
+              event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate; 
+         }
+     }); 
+	 
+	 for (var i = 0; i < songRows.length; i++) {
+         songRows[i].addEventListener('mouseleave', function(event) {
+             
+		  // Selects first child element, which is the song-item-number element
+             this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');	 
+         });
+     }
+	 
+	 
+	 
+	};
+
+//  items 1 and items 4 using built-in DOM functions, but items 2 and items 3 require us to look up the DOM tree to select a parent element. To address this requirement, write a findParentByClassName function that keeps traversing the DOM upward until a parent with a specified class name is found.below code is Bloc solution.I'm working on my own code.
+
+
+var findParentByClassName = function(element, targetClass) {
+    if (element) {
+        var currentParent = element.parentElement;
+        while (currentParent.className !== targetClass && currentParent.className !== null) {
+            currentParent = currentParent.parentElement;
+        }
+        return currentParent;
+    }
+};
+
+
+//The findParentByClassName function enables us to write a larger function that will always return the song item. This method, which we'll call getSongItem, should take an element and, based on that element's class name(s), use a  switch statement that returns the element with the .song-item-number class.
+
+
+var getSongItem = function(element) {
+    switch (element.className) {
+        case 'album-song-button':
+        case 'ion-play':
+        case 'ion-pause':
+            return findParentByClassName(element, 'song-item-number');
+        case 'album-view-song-item':
+            return element.querySelector('.song-item-number');
+        case 'song-item-title':
+        case 'song-item-duration':
+            return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
+        case 'song-item-number':
+            return element;
+        default:
+            return;
+    }  
+};
