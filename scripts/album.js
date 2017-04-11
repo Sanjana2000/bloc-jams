95233a832b3e65251cdda76777c2454803fe7267
Sanjana2000 <sanjana100@gmail.com>
2017-04-10 20:50:53 -0700

    modifying album.js and album.css using event delegation,event bubbling, HTML data attributes.Checkpoint-2.12 completed

--- scripts/album.js
+++ scripts/album.js
@@ -1,41 +1,41 @@
 // These are two album objects ( my temporary database)for this Album.In real senario,I'll pull hundreds or thousands from other database.These objects store info about songs,ex;title,artist,label,songs etc.
 
 // Example Album
- var albumPicasso = {
-     title: 'The Colors',
-     artist: 'Pablo Picasso',
-     label: 'Cubism',
-     year: '1881',
-     albumArtUrl: 'assets/images/album_covers/01.png',
-     songs: [
-         { title: 'Blue', duration: '4:26' },
-         { title: 'Green', duration: '3:14' },
-         { title: 'Red', duration: '5:01' },
-         { title: 'Pink', duration: '3:21'},
-         { title: 'Magenta', duration: '2:15'}
-     ]
+var albumPicasso = {
+        title: 'The Colors',
+        artist: 'Pablo Picasso',
+        label: 'Cubism',
+        year: '1881',
+        albumArtUrl: 'assets/images/album_covers/01.png',
+        songs: [
+              { title: 'Blue', duration: '4:26' },
+              { title: 'Green', duration: '3:14' },
+              { title: 'Red', duration: '5:01' },
+              { title: 'Pink', duration: '3:21'},
+              { title: 'Magenta', duration: '2:15'}
+             ]
  };
 
 // Another Example Album
  var albumMarconi = {
-     title: 'The Telephone',
-     artist: 'Guglielmo Marconi',
-     label: 'EM',
-     year: '1909',
-     albumArtUrl: 'assets/images/album_covers/20.png',
+         title: 'The Telephone',
+         artist: 'Guglielmo Marconi',
+         label: 'EM',
+         year: '1909',
+        albumArtUrl: 'assets/images/album_covers/20.png',
      songs: [
-         { title: 'Hello, Operator?', duration: '1:01' },
-         { title: 'Ring, ring, ring', duration: '5:01' },
-         { title: 'Fits in your pocket', duration: '3:21'},
-         { title: 'Can you hear me now?', duration: '3:14' },
-         { title: 'Wrong phone number', duration: '2:15'}
+            { title: 'Hello, Operator?', duration: '1:01' },
+            { title: 'Ring, ring, ring', duration: '5:01' },
+            { title: 'Fits in your pocket', duration: '3:21'},
+            { title: 'Can you hear me now?', duration: '3:14' },
+            { title: 'Wrong phone number', duration: '2:15'}
      ]
  };
 
-var createSongRow = function(songNumber, songName, songLength) {
+  var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
-      + '  <td class="song-item-number">' + songNumber + '</td>'
+      + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
       + '  <td class="song-item-title">' + songName + '</td>'
       + '  <td class="song-item-duration">' + songLength + '</td>'
       + '</tr>'
@@ -44,7 +44,7 @@ var createSongRow = function(songNumber, songName, songLength) {
      return template;
  };
 
-var setCurrentAlbum = function(album) {
+ var setCurrentAlbum = function(album) {
      // #1
      var albumTitle = document.getElementsByClassName('album-view-title')[0];
      var albumArtist = document.getElementsByClassName('album-view-artist')[0];
@@ -66,9 +66,30 @@ var setCurrentAlbum = function(album) {
          albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
      }
  };
+
+// Elements we'll be adding listeners to
+   var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+   var songRows = document.getElementsByClassName('album-view-song-item');
+
+   // Album button templates
+ var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
  
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
  };
 
-	
+    songListContainer.addEventListener('mouseover', function(event) {
+         // #1
+         // Only target individual song rows during event delegation
+         if (event.target.parentElement.className === 'album-view-song-item') {
+               event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+         }
+     });	
+
+     for (var i = 0; i < songRows.length; i++) {
+         songRows[i].addEventListener('mouseleave', function(event) {
+            // Selects first child element, which is the song-item-number element
+             this.children[0].innerHTML = this.children[0].getAttribute('data-song-number'); 
+         });
+     }
+
