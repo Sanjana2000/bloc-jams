6bfe40daf7d0b2a7ff8899a7a0c4e0a3aec0e7a7
Sanjana2000 <sanjana100@gmail.com>
2017-07-12 23:39:48 -0700

    modified landing.js

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,15 +1,15 @@
-var animatePoints = function() {
+ var pointsArray = document.getElementsByClassName('point');
  
-     var points = document.getElementsByClassName('point');
+ var animatePoints = function (points) {
  
-     var revealFirstPoint = function() {
+     var revealFirstPoint = function () {
          points[0].style.opacity = 1;
          points[0].style.transform = "scaleX(1) translateY(0)";
          points[0].style.msTransform = "scaleX(1) translateY(0)";
          points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
      };
  
-     var revealSecondPoint = function() {
+     var revealSecondPoint = function () {
          points[1].style.opacity = 1;
          points[1].style.transform = "scaleX(1) translateY(0)";
          points[1].style.msTransform = "scaleX(1) translateY(0)";
@@ -28,3 +28,21 @@ var animatePoints = function() {
      revealThirdPoint();
  
  };
+
+window.onload = function() {
+
+// Automatically animate the points on a tall screen where scrolling can't trigger the animation
+     if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+     window.addEventListener('scroll', function(event) {
+
+          if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);
+         }
+    });
