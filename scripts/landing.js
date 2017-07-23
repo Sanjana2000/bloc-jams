b5363c768d1c92d4d7fc703266c806b64bd1e41d
Sanjana2000 <sanjana100@gmail.com>
2017-07-23 10:33:26 -0700

    modified js files

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,14 +1,14 @@
  var pointsArray = document.getElementsByClassName('point');
- var revealPoint = function(index) {
-              points[index].style.opacity = 1;
-              points[index].style.transform = "scaleX(1) translateY(0)";
-              points[index].style.msTransform = "scaleX(1) translateY(0)";
-              points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+ var revealPoint = function(point) {
+              points.style.opacity = 1;
+              points.style.transform = "scaleX(1) translateY(0)";
+              points.style.msTransform = "scaleX(1) translateY(0)";
+              points.style.WebkitTransform = "scaleX(1) translateY(0)";
         };
      var animatePoints = function(points) {
-      for (var i = 0; i < points.length;i++) {
-           revealPoint(i);
-        }
+          forEach(points, revealPoints);
+      
+      
    };
     /* var revealFirstPoint = function() {
          points[0].style.opacity = 1;
