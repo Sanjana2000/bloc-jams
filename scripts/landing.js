c0acb35c310d933d0ee9acb4fb54b61446bcffec
Sanjana2000 <sanjana100@gmail.com>
2017-07-24 20:39:52 -0700

    recovered files

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,88 +1,19 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
- var pointsArray = document.getElementsByClassName('point');
+ var animatePoints = function() {
 
- var animatePoints = function(points) {
-=======
-var animatePoints = function() {
- 
-     var points = document.getElementsByClassName('point');
->>>>>>> checkpoint9.1
- 
-     var revealFirstPoint = function() {
-         points[0].style.opacity = 1;
-         points[0].style.transform = "scaleX(1) translateY(0)";
-         points[0].style.msTransform = "scaleX(1) translateY(0)";
-         points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-     };
- 
-     var revealSecondPoint = function() {
-         points[1].style.opacity = 1;
-         points[1].style.transform = "scaleX(1) translateY(0)";
-         points[1].style.msTransform = "scaleX(1) translateY(0)";
-         points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-     };
- 
-     var revealThirdPoint = function() {
-         points[2].style.opacity = 1;
-         points[2].style.transform = "scaleX(1) translateY(0)";
-         points[2].style.msTransform = "scaleX(1) translateY(0)";
-         points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-     };
- 
-     revealFirstPoint();
-     revealSecondPoint();
-     revealThirdPoint();
- 
-<<<<<<< HEAD
- };
-=======
-  var pointsArray = document.getElementsByClassName('point');
-       var revealPoint = function (point) {
-               points.style.opacity = 1;
-               points.style.transform = "scaleX(1) translateY(0)";
-               points.style.msTransform = "scaleX(1) translateY(0)";
-               points.style.WebkitTransform = "scaleX(1) translateY(0)";
-         };
-      var animatePoints = function (points) {
-           forEach(points, revealPoints);
+      var points = document.getElementsByClassName('point');
 
->>>>>>> checkpoint10
+ var revealPoint =function(index){
+          points[index].style.opacity = 1;
+          points[index].style.transform = "scaleX(1) translateY(0)";
+          points[index].style.msTransform = "scaleX(1) translateY(0)";
+          points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
 
-    };
+ }
 
-<<<<<<< HEAD
-// Automatically animate the points on a tall screen where scrolling can't trigger the animation
-     if (window.innerHeight > 950) {
-         animatePoints(pointsArray);
-     }
 
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+ for (var i = 0; i < points.length; i++) {
 
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+ revealPoint(i);
+ }
 
-    window.addEventListener('scroll', function (event) {
-
-        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-            animatePoints(pointsArray);
-        }
-        );
   };
-=======
- };
->>>>>>> checkpoint9.1
-=======
- window.onload = function () {
- // Automatically animate the points on a tall screen where scrolling can't trigger the animation
-      if (window.innerHeight > 950) {
-          animatePoints(pointsArray);
-      }
-     var sellingPoints = document.getElementsByClassName('selling-points')[0];
-     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-     window.addEventListener('scroll', function (event) {
-         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-             animatePoints(pointsArray);
-         }
-     });
-   };
->>>>>>> checkpoint10
