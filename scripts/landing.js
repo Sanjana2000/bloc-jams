c79b5203295dbec2e75fcf899c49339bcb61e6c4
Sanjana2000 <sanjana100@gmail.com>
2017-07-27 18:34:25 -0700

    checkpoint16 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -5,33 +5,15 @@
       var points = document.getElementsByClassName('point');
 
 
-      var revealFirstPoint = function () {
-          points[0].style.opacity = 1;
-          points[0].style.transform = "scaleX(1) translateY(0)";
-          points[0].style.msTransform = "scaleX(1) translateY(0)";
-          points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-      };
-
-      var revealSecondPoint = function () {
-          points[1].style.opacity = 1;
-          points[1].style.transform = "scaleX(1) translateY(0)";
-          points[1].style.msTransform = "scaleX(1) translateY(0)";
-          points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-      };
-
-      var revealThirdPoint = function () {
-          points[2].style.opacity = 1;
-          points[2].style.transform = "scaleX(1) translateY(0)";
-          points[2].style.msTransform = "scaleX(1) translateY(0)";
-          points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-      };
-
-      revealFirstPoint();
-      revealSecondPoint();
-      revealThirdPoint();
-
-
-     };
+    var revealPoint = function() {
+         // #7
+         $(this).css({
+             opacity: 1,
+             transform: 'scaleX(1) translateY(0)'
+         });
+    };
+   };
+    $.each($('.point'), revealPoint);
 
      var pointsArray = document.getElementsByClassName('point');
 
